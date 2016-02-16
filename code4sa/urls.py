b46111333a8d99f80e143af7a1463839b77670e3
Greg Kempe <greg@kempe.net>
2016-02-16 18:16:15 +0200

    taxtool -> budget2016

--- code4sa/urls.py
+++ code4sa/urls.py
@@ -5,8 +5,7 @@ from django.contrib import admin
 urlpatterns = patterns('',
     url(r'^$', view=lambda r: HttpResponse("Hello :)", content_type="text/plain")),
 
-    url(r'^token/$', 'code4sa.views.token'),
-    url(r'^submit/taxtool/$', 'code4sa.views.submit', kwargs={'project': 'taxtool'}),
+    url(r'^submit/budget2016/$', 'code4sa.views.submit', kwargs={'project': 'budget2016'}),
 
     url(r'^admin/', include(admin.site.urls)),
 )
