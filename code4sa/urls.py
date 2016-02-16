37530bf8fe5aaca9798516d11ec26d6a849835df
Greg Kempe <greg@kempe.net>
2016-02-16 18:09:20 +0200

    Simple view URL.

--- code4sa/urls.py
+++ code4sa/urls.py
@@ -1,8 +1,9 @@
+from django.http import HttpResponse
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^$', 'code4sa.views.home', name='home'),
+    url(r'^$', view=lambda r: HttpResponse("Hello :)", content_type="text/plain")),
 
     url(r'^token/$', 'code4sa.views.token'),
     url(r'^submit/taxtool/$', 'code4sa.views.submit', kwargs={'project': 'taxtool'}),
